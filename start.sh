f58ba3b7fc02c04822ede873b405463f13849b76
Andrew-O39 <andyowan.aoa@gmail.com>
2025-11-10 08:25:36 +0100

    Fixing network

--- start.sh
+++ start.sh
@@ -1,20 +1,19 @@
 #!/usr/bin/env sh
 set -eu
 
-# Make uvicorn/gunicorn trust proxy headers coming from the reverse proxy
-# (Caddy/Nginx set X-Forwarded-*; we allow all since traffic is internal)
+# Allow proxied headers (Gunicorn understands this flag)
+# (Caddy/Nginx set X-Forwarded-*, and this tells Gunicorn to trust them)
+# You can keep these envs, but they're not required by Gunicorn:
 export PROXY_HEADERS=1
 export FORWARDED_ALLOW_IPS="*"
 
-# 1) Run DB migrations (alembic.ini in the repo)
+# 1) Run DB migrations
 alembic upgrade head
 
 # 2) Start the app via Gunicorn + Uvicorn workers
-#    --forwarded-allow-ips lets Gunicorn honor X-Forwarded-* from our proxy
 exec gunicorn main:app \
   --workers 2 \
   --worker-class uvicorn.workers.UvicornWorker \
   --bind 0.0.0.0:8000 \
   --timeout 60 \
-  --forwarded-allow-ips="*" \
-  --proxy-headers
+  --forwarded-allow-ips="*"
